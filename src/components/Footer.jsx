016988d991de2a1839e5aa30fe27743e5d615dbf
arxllannn <arslanrajput386@gmail.com>
2025-08-13 02:18:54 +0500

    code updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className="bg-black text-white p-6 text-center">
-      <p className="text-sm">&copy; {new Date().getFullYear()} Zoox Clone. Built for educational purposes.</p>
+      <p className="text-sm">&copy; {new Date().getFullYear()} Zoox Clone.</p>
     </footer>
   )
 }
